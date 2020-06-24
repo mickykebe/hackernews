c339c49a184f8bbb9072b7163632bec5440cb5d5
mickykebe <mickykebe@gmail.com>
2020-06-24 17:13:02 +0300

    added story detail skeleton

--- src/components/storydetail.tsx
+++ src/components/storydetail.tsx
@@ -11,9 +11,18 @@ import { useWindowSize } from "../hooks/useWindowSize";
 import { BackButton } from "./backbutton";
 import { useParams, useHistory } from "react-router-dom";
 import { FiExternalLink as LinkIcon } from "react-icons/fi";
+import skeletonStyles from "./storydetailskeleton.module.css";
 
 interface Props {
   id: number;
+}
+
+interface SkeletonProps {
+  children?: React.ReactElement;
+}
+
+function StoryDetailSekeleton(props: SkeletonProps) {
+  return <div className={skeletonStyles.root} {...props} />;
 }
 
 export function StoryDetail({ id }: Props) {
@@ -21,37 +30,42 @@ export function StoryDetail({ id }: Props) {
   const { width } = useWindowSize();
   const { page } = useParams();
   const history = useHistory();
-  if (!story) {
-    return <div>Loading</div>;
-  }
   const handleBackClick = () => {
     let currentPage = page || "top";
     history.push(`/${currentPage}`);
   };
   return (
     <div className={styles.root}>
-      {(width as number) < 960 && <BackButton onClick={handleBackClick} />}
-      <div className={styles.head}>
-        <p className={styles.title}>{story.title}</p>
-        <div className={styles.storyDetails}>
-          <span className={styles.storyDetailItem}>
-            <UpIcon className={styles.storyDetailIcon} />
-            {story.score}
-          </span>
-          <span className={styles.storyDetailItem}>
-            <AiOutlineUser className={styles.storyDetailIcon} />
-            {story.by}
-          </span>
-          <span className={styles.storyDetailItem}>
-            <CommentIcon className={styles.storyDetailIcon} />{" "}
-            {story.descendants}
-          </span>
-        </div>
-        <a className={styles.storyLink} target="__blank" href={story.url}>
-          <LinkIcon /> {story.url}
-        </a>
-      </div>
-      <Comments ids={story.kids} />
+      <StoryDetailSekeleton>
+        {story && (
+          <React.Fragment>
+            {(width as number) < 960 && (
+              <BackButton onClick={handleBackClick} />
+            )}
+            <div className={styles.head}>
+              <p className={styles.title}>{story.title}</p>
+              <div className={styles.storyDetails}>
+                <span className={styles.storyDetailItem}>
+                  <UpIcon className={styles.storyDetailIcon} />
+                  {story.score}
+                </span>
+                <span className={styles.storyDetailItem}>
+                  <AiOutlineUser className={styles.storyDetailIcon} />
+                  {story.by}
+                </span>
+                <span className={styles.storyDetailItem}>
+                  <CommentIcon className={styles.storyDetailIcon} />{" "}
+                  {story.descendants}
+                </span>
+              </div>
+              <a className={styles.storyLink} target="__blank" href={story.url}>
+                <LinkIcon /> {story.url}
+              </a>
+            </div>
+            <Comments ids={story.kids} />
+          </React.Fragment>
+        )}
+      </StoryDetailSekeleton>
     </div>
   );
 }

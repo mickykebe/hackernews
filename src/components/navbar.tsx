0904ba6bc84024f2bede393b5cbbdf90a5ff5fcf
mickykebe <mickykebe@gmail.com>
2020-06-25 22:36:43 +0300

    added hamburger nav to mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { Link, useParams } from "react-router-dom";
 import { Logo } from "./logo";
 import styles from "./navbar.module.css";
 import { useCategory } from "../hooks/useCategory";
+import { NavMenu } from "./navmenu";
 
 const pageNames: { [key in StoryCategory]: string } = {
   topstories: "The Front Page",
@@ -14,28 +14,10 @@ const pageNames: { [key in StoryCategory]: string } = {
 
 export function NavBar() {
   const category = useCategory();
-  const { itemId } = useParams();
-  const itemIdParam = itemId ? `${itemId}` : "";
   return (
     <nav className={styles.root}>
       <Logo pageName={pageNames[category]} />
-      <ul className={styles.links}>
-        <li>
-          <Link to={`/top/${itemIdParam}`}>top</Link>
-        </li>
-        <li>
-          <Link to={`/new/${itemIdParam}`}>new</Link>
-        </li>
-        <li>
-          <Link to={`/ask/${itemIdParam}`}>ask</Link>
-        </li>
-        <li>
-          <Link to={`/show/${itemIdParam}`}>show</Link>
-        </li>
-        <li>
-          <Link to={`/jobs/${itemIdParam}`}>jobs</Link>
-        </li>
-      </ul>
+      <NavMenu />
     </nav>
   );
 }
---
+++ src/components/navmenu.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { Link, useParams } from "react-router-dom";
+import styles from "./navmenu.module.css";
+import { useWindowSize } from "../hooks/useWindowSize";
+
+export function NavMenu() {
+  const { itemId } = useParams();
+  const { width } = useWindowSize();
+  let itemIdParam = "";
+  if (itemId && width && width > 500) {
+    itemIdParam = `${itemId}`;
+  }
+  const [menuToggled, setMenuToggled] = React.useState(false);
+  const closeMenu = () => {
+    setMenuToggled(false);
+  };
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        className={styles.toggle}
+        checked={menuToggled}
+        onChange={(ev) => {
+          setMenuToggled(ev.target.checked);
+        }}
+      />
+      <div className={styles.hamburger}>
+        <span></span>
+      </div>
+      <div className={styles.linksContainer}>
+        <ul className={styles.links}>
+          <li>
+            <Link to={`/top/${itemIdParam}`} onClick={closeMenu}>
+              top
+            </Link>
+          </li>
+          <li>
+            <Link to={`/new/${itemIdParam}`} onClick={closeMenu}>
+              new
+            </Link>
+          </li>
+          <li>
+            <Link to={`/ask/${itemIdParam}`} onClick={closeMenu}>
+              ask
+            </Link>
+          </li>
+          <li>
+            <Link to={`/show/${itemIdParam}`} onClick={closeMenu}>
+              show
+            </Link>
+          </li>
+          <li>
+            <Link to={`/jobs/${itemIdParam}`} onClick={closeMenu}>
+              jobs
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}

a470dd1dee7dca6a69df66a1070c05ab512d2368
mickykebe <mickykebe@gmail.com>
2020-06-26 15:18:25 +0300

    added firebase analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,29 @@
 import * as firebase from "firebase/app";
 import "firebase/database";
+import "firebase/analytics";
 
 class Firebase {
   private static _instance: Firebase;
   private db: firebase.database.Reference;
+  private analytics: firebase.analytics.Analytics;
   private constructor() {
     const config = {
       databaseURL: "https://hacker-news.firebaseio.com",
     };
-    firebase.initializeApp(config);
-    this.db = firebase.database().ref("/v0");
+    const myAppConfig = {
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DB_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_APP_ID,
+      measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    };
+    const hnApp = firebase.initializeApp(config, "hnapp");
+    const myApp = firebase.initializeApp(myAppConfig, "myapp");
+    this.db = hnApp.database().ref("/v0");
+    this.analytics = myApp.analytics();
   }
   static getInstance() {
     if (!this._instance) {
